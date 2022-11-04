31cae26eadac50e97d9649ccbbf4784952132e07
WANWISAAA <chumpornw6@gmail.com>
2022-11-04 13:31:19 +0700

    update

---
+++ assignment.py
@@ -0,0 +1,19 @@
+#Eeample of assignment Operators
+a = 10
+#1. assign Value
+b = a
+print('1.Assign Value b = ', b)
+#2. Add And Assign Value
+b += a
+print ('2.Add and Assign Value b =',b)
+#3.Subtract And Assign Value
+b -= a
+print ('3.Subtract And Assign Value b =',b)
+#4.Multply and assign value
+b *= a
+print ('4.Multply and assign value b =',b)
+
+b /= a
+print ('5.Division and assign value b =',b)
+
+       
